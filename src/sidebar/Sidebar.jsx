fd538d3068ad03fce0b329319189d5d9ed88be87
Muchi Gora <muchigora@gmail.com>
2024-06-22 01:01:36 -0400

    sidebar completed

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,7 +3,47 @@ import "./sidebar.css";
 export default function Sidebar() {
   return (
     <div className="sidebar">
-        sidebar      
+        <div className="sidebarItem">
+
+            <span className="sidebarTitle">
+                ABOUT ME
+            </span>
+
+            <img className="sidebarImage" 
+                src="https://picsum.photos/200/300.webp" 
+                alt="" />
+            
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                
+            </p>
+
+            <span className="sidebarTitle">
+                CATEGORIES
+            </span>
+
+            <ul className="sidebarList">
+                <li className="sidebarListItem">Life</li>
+                <li className="sidebarListItem">Music</li>
+                <li className="sidebarListItem">Style</li>
+                <li className="sidebarListItem">Sport</li>
+                <li className="sidebarListItem">Cinema</li>
+                <li className="sidebarListItem">Tech</li>
+            </ul>
+
+        </div> 
+
+        <div className="sidebarItem">
+            <span className="sidebarTitle">FOLLOW US</span>
+            <div className="sidebarSocial">
+                <i className="sidebarIcon fa-brands fa-facebook"></i>
+                <i className="sidebarIcon fa-brands fa-square-x-twitter"></i>
+                <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+                <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+                <i className="sidebarIcon fa-brands fa-tiktok"></i>
+            </div>
+
+        </div>    
     </div>
   )
 }
