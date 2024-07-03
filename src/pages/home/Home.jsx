879e87343122a404890f27ada5d4e3320c48e763
Muchi Gora <muchigora@gmail.com>
2024-07-02 23:27:59 -0400

    single post created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
+import TopBar from "../../components/topbar/TopBar";
 import "./home.css";
 
 export default function Home() {
   return (
     <>
-        <Header/>  
+        <Header/>
         <div className="home">
             <Posts/>
             <Sidebar/>           
