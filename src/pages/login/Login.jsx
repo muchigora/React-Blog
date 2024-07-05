89b073a2ae62aa1c9aed7b9e803ad4c5cadae664
Muchi Gora <muchigora@gmail.com>
2024-07-04 23:29:53 -0400

    login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,22 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+        <span className="loginTitle">Login</span>
+
+        <form className='loginForm'>
+            <label>email</label>
+            <input type="Email" placeholder='Enter your email address'/>
+            <label>Password</label>
+            <input type="password" placeholder='Enter your password'/>
+
+            <button className='loginButton'>Login</button>
+    
+        </form>
+
+        <button className='registerButton'>Register</button>
+      
+    </div>
+  )
+}
